80a6eab3c831f987101d9f90d36792d2e11c6687
Benyu Zhang <byzhang@gmail.com>
2015-05-09 22:32:14 -0700

    change to cuda 7.0

--- zprezto/zsh/0_path.zsh
+++ zprezto/zsh/0_path.zsh
@@ -1,5 +1,5 @@
 # path, the 0 in the filename causes this to load first
-export PATH=/usr/local/cuda-6.5/bin:/opt/local/libexec/gnubin:$PATH:$HOME/.zprezto/bin
+export PATH=/usr/local/cuda-7.0/bin:/opt/local/libexec/gnubin:$PATH:$HOME/.zprezto/bin
 export PYTHONPATH=$PYTHONPATH:$HOME/src
 export MLPYTHON_DATASET_REPO=$HOME/src/DocNADE/ml_datasets
 # export LD_PRELOAD=/usr/local/lib/libhoard.so
